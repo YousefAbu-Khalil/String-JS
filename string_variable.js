c67a57d2e093847824cbad2a48596d04c38520ad
YousefAbu-Khalil <Yousefabukhalil525@gmail.com>
2024-06-02 20:20:44 +0300

    Add files via upload

---
+++ string_variable.js
@@ -0,0 +1,141 @@
+ 0// 1-  Declare a variable called "name" and assign it the value "John". 
+ /******* Start Your Code *********/
+  
+/* var name="john Doe";
+document.getElementById("demo").innerHTML = name;*/
+
+ /******* End Your Code ********* */
+
+
+// 2- Declare a constant called "PI" and assign it the value 3.14.
+ /******* Start Your Code *********/
+  /*const PI= 3.14;
+document.getElementById("demo").innerHTML = PI;*/
+
+
+ /******* End Your Code ********* */
+
+// 3- Declare a variable called "age" and assign it your age.
+ /******* Start Your Code *********/
+  
+/*var age= 23;
+document.getElementById("demo").innerHTML =age;*/
+
+ /******* End Your Code ********* */
+
+// 4- Declare a variable called "isStudent" and assign it a boolean value.
+ /******* Start Your Code *********/
+  
+/*var isStudent=true; 
+document.getElementById("demo").innerHTML =isStudent;*/
+
+ /******* End Your Code ********* */
+
+// 5- Create a string variable called "message" and assign it a message of your choice.
+ /******* Start Your Code *********/
+  
+ /*var message ="Hi";
+document.getElementById("demo").innerHTML =message;*/
+
+
+ /******* End Your Code ********* */
+
+// 6- Use template literals to create a string that includes the variables "name" and "age".
+ /******* Start Your Code *********/
+  
+/*var name ="yousef";
+var age=23;
+document.getElementById("demo").innerHTML = name + "    "+  age;*/
+
+ /******* End Your Code ********* */
+
+// 7- Use the string method "length" to find the length of the string "message".
+ /******* Start Your Code *********/
+  
+/*var message="Hi I,m yousef";
+document.getElementById("demo").innerHTML =message.length;*/
+
+ /******* End Your Code ********* */
+
+// 8- Use the string method "toUpperCase()" to convert the string "message" to uppercase. 
+/******* Start Your Code *********/
+  
+/*var message="Hi I,m yousef";
+document.getElementById("demo").innerHTML =message.toUpperCase();*/
+
+ /******* End Your Code ********* */
+
+// 9- Use the string method "toLowerCase()" to convert the string "message" to lowercase.
+ /******* Start Your Code *********/
+  
+/*var message="Hi I,m yousef";
+document.getElementById("demo").innerHTML = message.toLowerCase();*/
+
+ /******* End Your Code ********* */
+
+// 10- Use the string method "indexOf()" to find the position of the word "JavaScript" in the string "I love learning JavaScript".
+ /******* Start Your Code *********/
+  
+/*var message="I love learning JavaScript";
+document.getElementById("demo").innerHTML = message.indexOf("JavaScript");*/
+
+
+ /******* End Your Code ********* */
+
+// 11- Use the string method "slice()" to extract a portion of the string "message".
+ /******* Start Your Code *********/
+  
+/*var car=["bmw" , "hunday" , "mazda","marceds"];
+document.getElementById("demo").innerHTML = car.slice(2,4);*/
+
+ /******* End Your Code ********* */
+
+// 12- Declare a variable called "num1" and assign it a number.
+ /******* Start Your Code *********/
+ 
+/*var num1=40;
+document.getElementById("demo").innerHTML=num1;*/
+
+/*var num1;
+num1=40;
+document.getElementById("demo").innerHTML=num1;*/
+
+ /******* End Your Code ********* */
+
+// 13- Declare a variable called "num2" and assign it another number.
+ /******* Start Your Code *********/
+/*
+ var num2;
+ num2=50;
+document.getElementById("demo").innerHTML=num2;
+*/  
+
+ /*var num2=50;
+ document.getElementById("demo").innerHTML=num2;*/
+
+ /******* End Your Code ********* */
+
+// 14- Use the arithmetic operator "+" to add num1 and num2.
+ /******* Start Your Code *********/
+  
+ /*var num1=40;
+var num2=50;
+document.getElementById("demo").innerHTML=(num1 + num2);*/
+
+
+ /******* End Your Code ********* */
+
+// 15- Use the arithmetic operator "-" to subtract num1 from num2.
+ /******* Start Your Code *********/
+  
+/*var num1=40;
+var num2=50;
+document.getElementById("demo").innerHTML=(num1 - num2);*/
+
+ /******* End Your Code ********* */
+ 
+
+
+
+
+
